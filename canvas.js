1e14f3f28d779ce37ff533cc64ca6959bbb60cd8
Kenneth To <kennethkto@gmail.com>
2015-05-06 10:47:23 -0600

    add onclick squares for red and blue rectangles on canvas element

--- canvas.js
+++ canvas.js
@@ -1,6 +1,29 @@
-// function drawBorder(){
-//     var canvas = document.getElementById("canvas");
-//     var context = canvas.getContext('2d');
-//     var size = { x: canvas.width, y: canvas.height }
-//     context.strokeRect(0,0, size.x, size.y);
-// }
+function drawBorder(){
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext('2d');
+    var size = { x: canvas.width, y: canvas.height };
+    context.strokeRect(0,0, size.x, size.y);
+}
+
+drawBorder();
+
+function drawRedRectangle(){
+  var canvas = document.getElementById("canvas");
+  var context = canvas.getContext('2d');
+  var size = {x: canvas.width, y: canvas.height};
+  context.fillStyle='red';
+  context.fillRect(size.x/2,size.y/2,size.x,size.y);
+
+}
+
+
+
+function drawBlueRectangle(){
+  var canvas = document.getElementById("canvas");
+  var context = canvas.getContext('2d');
+  var size = {x: canvas.width, y: canvas.height };
+
+  context.fillStyle='blue';
+  context.fillRect(0,0,size.x/2,size.y/2);
+
+}
